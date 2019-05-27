a57aef6add5b05a371223f99b9305181b345d7ae
Reysmer Valle <reysmerwvr@gmail.com>
2019-05-27 00:01:49 -0500

    Updates

---
+++ precache-manifest.62bbd8908778ddb33d31e2a38af0c85a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff304442aac97ad033c96d351fbb9dfd",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "adec61b56f15cf96b848",
+    "url": "/pokedex/static/css/2.b1119605.chunk.css"
+  },
+  {
+    "revision": "fd9402b25a7039d335e4",
+    "url": "/pokedex/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "adec61b56f15cf96b848",
+    "url": "/pokedex/static/js/2.d9c8b5bb.chunk.js"
+  },
+  {
+    "revision": "fd9402b25a7039d335e4",
+    "url": "/pokedex/static/js/main.28fafda6.chunk.js"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  }
+]);
